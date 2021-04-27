c3dbf1af1da3703ebec70099806ff4806ca856f7
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-04-27 09:09:54 -0300

    add valor zero

---
+++ chapter 01/06 - valor zero.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a int     // 0
+var b float64 // 0
+var c bool    // false
+var d string  // ""
+// pointers, functions, interfaces, slices, channels, maps: nil
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
