38b44456b2266360d352e55f7bee3f70c531a453
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-04-27 09:33:29 -0300

    add tipos customizados

---
+++ chapter 01/09 - tipos customizados.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type hotdog int
+
+var b hotdog = 10
+
+func main() {
+	x := 10
+
+	fmt.Printf("%v %T\n", x, x)
+	fmt.Printf("%v %T\n", b, b)
+
+	// x = b  // error: cannot use b (type hotdog) as type int in assignment
+	// b = x  // error: cannot use x (type int) as type hotdog in assignment
+}
