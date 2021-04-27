1d5ffaaa779061ac1b337f5cba3128a25df10132
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-04-27 09:54:03 -0300

    add exercise 03

---
+++ chapter 02/exercise 03.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+
+func main() {
+	s := fmt.Sprint(x, y, z)
+
+	fmt.Println(s)
+	fmt.Printf("x=%v, y=\"%v\", z=%v\n", x, y, z)
+}
