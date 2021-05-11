7e8555b5e652661726709e0c56a48ae82d8cb48c
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-05-11 14:05:47 -0300

    add exercise 03

---
+++ chapter 07/exercise 03.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	anoemqueeunasci := 1988
+	anoateoqualeuquerocontar := 2088
+
+	for anoemqueeunasci <= anoateoqualeuquerocontar {
+		fmt.Println(anoemqueeunasci)
+		anoemqueeunasci++
+	}
+}
