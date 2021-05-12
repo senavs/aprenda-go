37a370c3cfa1f729780c10f51d07519bc23364bf
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-05-12 08:33:46 -0300

    add exercise 07

---
+++ chapter 07/exercise 07.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	tamanhodocansaço := 0
+	if tamanhodocansaço == 0 {
+		fmt.Println("que malandragem")
+	} else if tamanhodocansaço == 1 {
+		fmt.Println("uma gelada ia bem")
+	} else {
+		fmt.Println("ih já era, só nascendo denovo")
+	}
+}
